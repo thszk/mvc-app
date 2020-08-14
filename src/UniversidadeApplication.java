42652c9c62e3e57c51c0f941a6134d9139b5e344
thiago.lodi <thiago.lodi@azi.com.br>
2020-08-13 20:33:49 -0400

    aula 1: adicionei model de aluno com getters e setters

--- src/UniversidadeApplication.java
+++ src/UniversidadeApplication.java
@@ -1,5 +1,17 @@
+import model.AlunoM;
+
 public class UniversidadeApplication {
     public static void main(String[] args) {
+        AlunoM aluno = new AlunoM();
 
+        aluno.setRa(165478);
+        aluno.setCodigoCurso(130);
+        aluno.setGrade(2016);
+        aluno.setTurno("Noturno");
+        aluno.setNome("Thiago");
+
+        System.out.println("nome: " + aluno.getNome());
+        System.out.println("ra: " + aluno.getRa());
+        System.out.println("turno: " + aluno.getTurno());
     }
 }
---
+++ src/model/AlunoM.java
@@ -0,0 +1,49 @@
+package model;
+
+public class AlunoM {
+    private int ra;
+    private int codigoCurso;
+    private String nome;
+    private String turno;
+    private int grade;
+
+    public int getRa() {
+        return ra;
+    }
+
+    public void setRa(int ra) {
+        this.ra = ra;
+    }
+
+    public int getCodigoCurso() {
+        return codigoCurso;
+    }
+
+    public void setCodigoCurso(int codigoCurso) {
+        this.codigoCurso = codigoCurso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+}
